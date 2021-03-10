c43a301db8a9c4dc1c4a37011155944449ae8429
Sandeep <sandeep.mahal6@gmail.com>
2021-03-10 15:46:47 -0500

    RideDetail updated

--- build/Pods.build/Debug-iphonesimulator/Pods-demo.build/Objects-normal/x86_64/Pods-demo-dummy.d
+++ build/Pods.build/Debug-iphonesimulator/Pods-demo.build/Objects-normal/x86_64/Pods-demo-dummy.d
@@ -1,6 +1,6 @@
 dependencies: \
-  /Users/sandy/Downloads/demo\ \ 2/Pods/Target\ Support\ Files/Pods-demo/Pods-demo-dummy.m \
-  /Users/sandy/Downloads/demo\ \ 2/build/Pods.build/Debug-iphonesimulator/Pods-demo.build/module.modulemap \
-  /Users/sandy/Downloads/demo\ /Pods/Firebase/CoreOnly/Sources/module.modulemap \
+  /Users/sandy/Downloads/demo2/Pods/Target\ Support\ Files/Pods-demo/Pods-demo-dummy.m \
+  /Users/sandy/Downloads/demo2/build/Pods.build/Debug-iphonesimulator/Pods-demo.build/module.modulemap \
+  /Users/sandy/Downloads/demo2/Pods/Firebase/CoreOnly/Sources/module.modulemap \
   /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.4.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
   /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.4.sdk/usr/include/mach-o/dyld.modulemap
